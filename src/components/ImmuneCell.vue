5b717df346de40c10695ae6e3a5035456dc8ecc6
Ruhshan <ruhshan.ahmed@gmail.com>
2022-02-25 00:31:49 +0600

    Add animated objects

---
+++ src/components/ImmuneCell.vue
@@ -0,0 +1,103 @@
+<template>
+  <transition name="fade">
+    <img
+      v-if="show"
+      :src="require('../assets/' + cellType + '.svg')"
+      :class="{base:true, clockwiseSpin:clockWiseSpin, antiClockwiseSpin:!clockWiseSpin}"
+      :style="{ left: leftPos + 'px', top: topPos + 'px' }"
+    />
+  </transition>
+</template>
+<script>
+export default {
+  name: "ImmuneCell",
+  props: {
+    cellType: String,
+  },
+  async mounted() {
+   
+    this.calculatePosition();
+    var wait = Math.floor(Math.random() * 5000);
+    
+    await this.sleep(wait);
+    this.clockWiseSpin = wait % 2 == 0 ? true: false
+    
+    this.show= true
+  },
+  methods: {
+    sleep: function (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    },
+    calculatePosition: function () {
+      var banner = document.getElementById("banner");
+      var maxHeight = banner.clientHeight;
+      var maxWidth = banner.clientWidth;
+
+      var randomHeight = Math.floor(Math.random() * maxHeight);
+      var randomWeight = Math.floor(Math.random() * maxWidth);
+
+      this.leftPos = randomWeight;
+      this.topPos = randomHeight;
+    },
+  },
+  data: function () {
+    return {
+      leftPos: 0,
+      topPos: 0,
+      show: false,
+      t: true,
+      clockWiseSpin:true  
+    };
+  },
+};
+</script>
+<style scoped>
+.base {
+  filter: invert(1);
+  height: 100px;
+  position: absolute;
+  
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transition: opacity 3s;
+}
+
+
+@keyframes clockwiseSpin {
+   from  {
+     transform: rotate(0deg);
+  }
+  to {
+     transform: rotate(360deg);
+   }	
+}
+
+@keyframes antiClockwiseSpin {
+   from  {
+     transform: rotate(360deg);
+  }
+  to {
+     transform: rotate(0deg);
+   }	
+}
+
+.clockwiseSpin {
+   animation-duration: 10s;
+   animation-iteration-count: infinite;
+   animation-name: clockwiseSpin;
+   animation-timing-function: linear;
+}
+
+.antiClockwiseSpin {
+   animation-duration: 10s;
+   animation-iteration-count: infinite;
+   animation-name: antiClockwiseSpin;
+   animation-timing-function: linear;
+}
+</style>
--- src/components/ImmuneSystem.vue
+++ src/components/ImmuneSystem.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="shape1">
-    <section class="intro home">
+    <section class="intro home" id="banner">
       <div class="wrap">
         <nav class="animated fadeIn">
           <a href="#book" class="active"><strong>বই সম্পর্কে</strong></a>
@@ -24,16 +24,29 @@
         <h2>শরীর যেভাবে রোগ প্রতিরোধ করে</h2>
         <h3>— <a href="#author">রুহশান আহমেদ</a></h3>
       </div>
-      <div class=" animated fadeIn"><!--shapes--></div>
+      <div class="animated fadeIn"></div>
+
+      <div v-for="n in 5" :key="n">
+        <p style="display: none">{{ n }}</p>
+        <immune-cell cellType="antibody" />
+        <immune-cell cellType="bacteria" />
+        <immune-cell cellType="virus" />
+      </div>
+
       <div class="icon-scroll animated fadeIn"><span>স্ক্রল</span></div>
     </section>
   </div>
 </template>
 
 <script>
+import ImmuneCell from "./ImmuneCell.vue";
+
 export default {
-  name: 'ImmuneSystem',
-}
+  name: "ImmuneSystem",
+  components: {
+    ImmuneCell,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
