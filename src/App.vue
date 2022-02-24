0ff159070f1c327463d2c83f764e323d819e8197
Ruhshan <ruhshan.ahmed@gmail.com>
2022-02-24 21:59:49 +0600

    Created immune system book component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <immune-system/>
+  </div>
+</template>
+
+<script>
+import ImmuneSystem from './components/ImmuneSystem.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+    ImmuneSystem
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+}
+</style>
---
+++ src/components/ImmuneSystem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="shape1">
+    <section class="intro home">
+      <div class="wrap">
+        <nav class="animated fadeIn">
+          <a href="#book" class="active"><strong>বই সম্পর্কে</strong></a>
+          <a
+            href="https://designsystemfoundations.com/design-systems-book/"
+            class="buy"
+            >Buy the book</a
+          >
+          <a
+            href="https://designsystemfoundations.com/design-systems-book/"
+            class="buy-mobile"
+            >Buy now</a
+          >
+        </nav>
+        <h1>
+          <span class="a-wrap"><span class="l1">ইমিউন </span></span>
+          <span class="a-wrap"><span class="l2">সিস্টেম </span></span>
+          <!-- <span class="a-wrap"><span class="l3">Found—</span></span>
+          <span class="a-wrap"><span class="l4">ations</span></span> -->
+        </h1>
+        <h2>শরীর যেভাবে রোগ প্রতিরোধ করে</h2>
+        <h3>— <a href="#author">রুহশান আহমেদ</a></h3>
+      </div>
+      <div class=" animated fadeIn"><!--shapes--></div>
+      <div class="icon-scroll animated fadeIn"><span>স্ক্রল</span></div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImmuneSystem',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
