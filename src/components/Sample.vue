08de780636da625913055a8fff552c0f2cbedf3c
Ruhshan <ruhshan.ahmed@gmail.com>
2022-02-25 16:41:46 +0600

    Improved book nav buttons

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,21 +1,36 @@
 <template>
   <section id="gallery" class="text">
     <h2><strong>পড়ে দেখুন</strong></h2>
-    <flipbook
-      v-slot="flipbook"
-      class="flipbook"
-      :ambient="1"
-      :pages="[
-        'https://i.postimg.cc/dVWhSn8G/cover.png',
-        'https://i.postimg.cc/nhSZBWJ9/page-1-1.jpg',
-        'https://i.postimg.cc/LXdcVK05/page-2-1.jpg',
-        'https://i.postimg.cc/ncdNPQw6/page-3-1.jpg',
-      ]"
-    >
-      <button @click="flipbook.flipLeft" style="padding-right: 20px">
-        আগের পৃষ্ঠা
-      </button>
-      <button @click="flipbook.flipRight">পরের পৃষ্ঠা</button>
+    <flipbook v-slot="flipbook" class="flipbook" :ambient="1" :pages="pages">
+      <div class="page-nav">
+        <font-awesome-layers full-width class="fa-3x">
+          <font-awesome-icon
+            class="ma-1 btn left"
+            :class="{ disabled: !flipbook.canFlipLeft }"
+            icon="chevron-circle-left"
+            @click="flipbook.flipLeft"
+          ></font-awesome-icon>
+          <font-awesome-layers-text
+            class="gray8"
+            transform="right-20 shrink-8"
+            value="পূর্ববর্তী"
+          />
+        </font-awesome-layers>
+
+        <font-awesome-layers full-width class="fa-3x">
+          <font-awesome-icon
+            class="ma-1 btn right"
+            :class="{ disabled: !flipbook.canFlipRight }"
+            icon="chevron-circle-right"
+            @click="flipbook.flipRight"
+          ></font-awesome-icon>
+          <font-awesome-layers-text
+            class="gray8"
+            transform="left-20 shrink-8"
+            value="পরবর্তী"
+          />
+        </font-awesome-layers>
+      </div>
     </flipbook>
   </section>
 </template>
@@ -25,6 +40,17 @@ import Flipbook from "flipbook-vue";
 export default {
   name: "Sample",
   components: { Flipbook },
+  data() {
+    return {
+      pages: [
+        null,
+        "https://i.postimg.cc/dVWhSn8G/cover.png",
+        "https://i.postimg.cc/nhSZBWJ9/page-1-1.jpg",
+        "https://i.postimg.cc/LXdcVK05/page-2-1.jpg",
+        "https://i.postimg.cc/ncdNPQw6/page-3-1.jpg",
+      ],
+    };
+  },
 };
 </script>
 
@@ -35,8 +61,14 @@ export default {
 
   background-color: blanchedalmond;
 }
-</style>
 
-https://i.postimg.cc/nhSZBWJ9/page-1-1.jpg
-https://i.postimg.cc/LXdcVK05/page-2-1.jpg
-https://i.postimg.cc/ncdNPQw6/page-3-1.jpg
+.flipbook .bounding-box {
+  box-shadow: 0 0 20px #000;
+}
+
+.page-nav {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronCircleLeft,
+  faChevronCircleRight,
+  faPlusCircle,
+  faMinusCircle
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon ,FontAwesomeLayers, FontAwesomeLayersText} from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faChevronCircleLeft,
+  faChevronCircleRight,
+  faPlusCircle,
+  faMinusCircle
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
+
+
 Vue.config.productionTip = false
 
 new Vue({
